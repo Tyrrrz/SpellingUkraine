303b09a1a6c9369090c82b5a78077d3a6c83ceaa
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-07-19 20:43:55 +0300

    Fix `useSessionState()`

--- web/hooks/useSessionState.ts
+++ web/hooks/useSessionState.ts
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 
 const getStorageValue = (key: string) => {
   const item = sessionStorage.getItem(key);
-  if (item) {
+
+  if (typeof item !== 'undefined' && item !== null) {
     try {
       return JSON.parse(item);
     } catch {
