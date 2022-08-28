b7464f83123932ee097f3e5491b55957c9ea514d
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-08-28 21:03:12 +0300

    asd

--- reddit-scan/index.ts
+++ reddit-scan/index.ts
@@ -51,12 +51,14 @@ const app = command({
     submissionSampling,
     commentSampling
   }) => {
+    console.log('Subreddits:', subreddits);
+    console.log('Vocabulary:', entryIds);
+    console.log('Submission sampling:', submissionSampling);
+    console.log('Comment sampling:', commentSampling);
+
     const before = new Date();
     const after = new Date(before.getTime() - interval * 1000);
     console.log('Scanning for messages in range:', { after, before });
-
-    console.log('Subreddits:', subreddits);
-    console.log('Vocabulary:', entryIds);
 
     const vocabulary = await Promise.all(entryIds.map(async (id) => await loadVocabularyEntry(id)));
 
@@ -76,73 +78,78 @@ const app = command({
     await Promise.all(
       subreddits.map(async (subreddit) => {
         for await (const post of reddit.getLatestPosts(subreddit, after, before)) {
-          postsProcessed++;
+          try {
+            if (post.author === me.name) {
+              continue;
+            }
 
-          if (post.author === me.name) {
-            continue;
-          }
+            const titleNormalized = post.kind === 'submission' ? post.title : '';
 
-          const titleNormalized = post.kind === 'submission' ? post.title : '';
+            // Scrub mentions, URLs, block quotes
+            const textNormalized = post.text
+              .replace(/\b\/?u\/\w+\b/g, '')
+              .replace(/\b\/?r\/\w+\b/g, '')
+              .replace(/\b(https?:\/\/)[^\s]*\b/g, '')
+              .replace(/^>.*$/gm, '');
 
-          // Scrub mentions, URLs, block quotes
-          const textNormalized = post.text
-            .replace(/\b\/?u\/\w+\b/g, '')
-            .replace(/\b\/?r\/\w+\b/g, '')
-            .replace(/\b(https?:\/\/)[^\s]*\b/g, '')
-            .replace(/^>.*$/gm, '');
+            const match = predicates.find((predicate) => {
+              const keywordPattern = new RegExp(`\\b${predicate.keyword}\\b`, 'gi');
 
-          const match = predicates.find((predicate) => {
-            const keywordPattern = new RegExp(`\\b${predicate.keyword}\\b`, 'gi');
+              const correctSpellingPattern = new RegExp(
+                `\\b${predicate.entry.correctSpelling}\\b`,
+                'gi'
+              );
 
-            const correctSpellingPattern = new RegExp(
-              `\\b${predicate.entry.correctSpelling}\\b`,
-              'gi'
+              return (
+                (keywordPattern.test(titleNormalized) &&
+                  !correctSpellingPattern.test(titleNormalized)) ||
+                (keywordPattern.test(textNormalized) &&
+                  !correctSpellingPattern.test(textNormalized))
+              );
+            });
+
+            if (!match) {
+              continue;
+            }
+
+            const sampling = post.kind === 'submission' ? submissionSampling : commentSampling;
+            if (Math.random() > sampling) {
+              continue;
+            }
+
+            console.log('Content:', post);
+            console.log('Match:', {
+              correct: match.entry.correctSpelling,
+              incorrect: match.keyword
+            });
+
+            const reply = await reddit.postReply(
+              post,
+              [
+                `💡 It's \`${match.entry.correctSpelling}\`, not \`${match.keyword}\`. `,
+                `Support Ukraine by using the correct spelling! `,
+                `[Learn more](https://spellingukraine.com/i/${match.entry.id}).`,
+                `\n\n___\n\n`,
+                `[^(Why spelling matters)](https://spellingukraine.com) `,
+                `^(|) `,
+                `[^(Stand with Ukraine)](https://stand-with-ukraine.pp.ua) `,
+                `^(|) `,
+                `^(I'm a bot, sorry if I'm missing context)`
+              ].join('')
             );
 
-            return (
-              (keywordPattern.test(titleNormalized) &&
-                !correctSpellingPattern.test(titleNormalized)) ||
-              (keywordPattern.test(textNormalized) && !correctSpellingPattern.test(textNormalized))
-            );
-          });
-
-          if (!match) {
-            continue;
+            console.log('Reply:', reply);
+            postsRepliedTo++;
+          } finally {
+            if (++postsProcessed % 10 === 0) {
+              console.log(`Processed ${postsProcessed} posts`);
+            }
           }
-
-          const sampling = post.kind === 'submission' ? submissionSampling : commentSampling;
-          if (Math.random() > sampling) {
-            continue;
-          }
-
-          console.log('Content:', post);
-          console.log('Match:', {
-            correct: match.entry.correctSpelling,
-            incorrect: match.keyword
-          });
-
-          const reply = await reddit.postReply(
-            post,
-            [
-              `💡 It's \`${match.entry.correctSpelling}\`, not \`${match.keyword}\`. `,
-              `Support Ukraine by using the correct spelling! `,
-              `[Learn more](https://spellingukraine.com/i/${match.entry.id}).`,
-              `\n\n___\n\n`,
-              `[^(Why spelling matters)](https://spellingukraine.com) `,
-              `^(|) `,
-              `[^(Stand with Ukraine)](https://stand-with-ukraine.pp.ua) `,
-              `^(|) `,
-              `^(I'm a bot, sorry if I'm missing context)`
-            ].join('')
-          );
-
-          console.log('Reply:', reply);
-          postsRepliedTo++;
         }
       })
     );
 
-    console.log('Finished scanning.');
+    console.log('Finished scanning');
     console.log('Posts processed:', postsProcessed);
     console.log('Posts replied to:', postsRepliedTo);
   }
