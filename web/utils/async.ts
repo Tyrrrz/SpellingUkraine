22564ccea507e42f441572809af5c6ec65847cb2
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2022-09-09 01:27:50 +0300

    Add callback to `bufferIterable`

--- web/utils/async.ts
+++ web/utils/async.ts
@@ -1,8 +1,12 @@
-export const bufferIterable = async <T>(generator: AsyncIterable<T>) => {
+export const bufferIterable = async <T>(
+  generator: AsyncIterable<T>,
+  callback?: (item: T) => void
+) => {
   const buffer: T[] = [];
 
   for await (const item of generator) {
     buffer.push(item);
+    callback?.(item);
   }
 
   return buffer;
