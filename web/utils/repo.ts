04ef1e1c7569d29922a6034d861e3759654a25e2
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2022-10-24 23:24:35 +0300

    Refactor

--- web/utils/repo.ts
+++ web/utils/repo.ts
@@ -8,21 +8,21 @@ export const getRepoCommitUrl = (commit: string) => {
   return joinUrl(getRepoUrl(), 'commit', commit);
 };
 
-export const getRepoFileUrl = (path: string, ref: string = 'master') => {
-  return joinUrl(getRepoUrl(), 'tree', ref, path);
+export const getRepoFileUrl = (path: string, options: { ref: string }) => {
+  return joinUrl(getRepoUrl(), 'tree', options.ref || 'master', path);
 };
 
-export const getRepoFileEditUrl = (path: string, ref: string = 'master') => {
-  return joinUrl(getRepoUrl(), 'edit', ref, path);
+export const getRepoFileEditUrl = (path: string, options: { ref: string }) => {
+  return joinUrl(getRepoUrl(), 'edit', options.ref || 'master', path);
 };
 
-type NewIssueOptions = {
-  template?: string;
-  labels?: string;
-  title?: string;
-  details?: string;
-};
-
-export const getRepoNewIssueUrl = (options: NewIssueOptions = {}) => {
+export const getRepoNewIssueUrl = (
+  options: {
+    template?: string;
+    labels?: string;
+    title?: string;
+    details?: string;
+  } = {}
+) => {
   return formatUrlWithQuery(joinUrl(getRepoUrl(), 'issues', 'new'), options);
 };
