3bcd2fe70ae4b849dc1c54d25994f7947b9dc7c8
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-11-24 14:36:54 +0000

    Minor cleanup

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,9 @@
 import Layout from '@/components/layout';
 import '@/pages/globals.css';
 import type { AppProps } from 'next/app';
+import { FC } from 'react';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
