9c0108e7bfd2e6b784b0d715be705c9b2aa5493f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-19 00:00:45 +0200

    Simplify `useTheme`

--- web/hooks/useTheme.ts
+++ web/hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import useLocalState from '~/hooks/useLocalState';
 import useMedia from '~/hooks/useMedia';
 
@@ -7,25 +7,15 @@ type Theme = 'light' | 'dark';
 const useTheme = () => {
   const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
   const [userPreferredTheme, setUserPreferredTheme] = useLocalState<Theme | null>('theme', null);
-  const [actualTheme, setActualTheme] = useState<Theme>(systemPreferredTheme);
-
-  // If the user has not set a preferred theme, use the system's preferred theme
-  useEffect(() => {
-    if (userPreferredTheme) {
-      setActualTheme(userPreferredTheme);
-    } else {
-      setActualTheme(systemPreferredTheme);
-    }
-  }, [systemPreferredTheme, userPreferredTheme]);
 
   return useMemo(() => {
     return {
       systemPreferredTheme,
       userPreferredTheme,
-      theme: actualTheme,
+      theme: userPreferredTheme || systemPreferredTheme,
       setTheme: setUserPreferredTheme
     };
-  }, [systemPreferredTheme, userPreferredTheme, setUserPreferredTheme, actualTheme]);
+  }, [systemPreferredTheme, userPreferredTheme, setUserPreferredTheme]);
 };
 
 export default useTheme;
