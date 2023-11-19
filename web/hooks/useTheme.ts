36969f22d725985b71dd5b7271b68490859adf50
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-19 19:35:10 +0200

    Simplify `useTheme`

--- web/hooks/useTheme.ts
+++ web/hooks/useTheme.ts
@@ -2,11 +2,15 @@ import { useMemo } from 'react';
 import useLocalState from '~/hooks/useLocalState';
 import useMedia from '~/hooks/useMedia';
 
-type Theme = 'light' | 'dark';
-
 const useTheme = () => {
-  const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
-  const [userPreferredTheme, setUserPreferredTheme] = useLocalState<Theme | null>('theme', null);
+  const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)')
+    ? ('dark' as const)
+    : ('light' as const);
+
+  const [userPreferredTheme, setUserPreferredTheme] = useLocalState<'light' | 'dark' | null>(
+    'theme',
+    null
+  );
 
   return useMemo(() => {
     return {
