11084a66bdf018e1ca27d4faf6ef5c5025137bfa
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-03-11 22:03:15 +0200

    Fix color themes and dark mode switcher

--- web/components/buttonLink.tsx
+++ web/components/buttonLink.tsx
@@ -22,15 +22,18 @@ const ButtonLink: FC<ButtonLinkProps> = ({
   return (
     <button
       type={submit ? 'submit' : 'button'}
-      className={c({
-        'text-ukraine-blue': variant === 'normal' && color === 'blue',
-        'dark:text-blue-300': variant === 'normal' && color === 'blue',
-        'text-ukraine-yellow': variant === 'normal' && color === 'yellow',
-        'hover:underline': variant === 'normal' && !disabled,
-        'hover:text-ukraine-blue': variant === 'discreet' && color === 'blue' && !disabled,
-        'dark:hover:text-blue-300': variant === 'discreet' && color === 'blue' && !disabled,
-        'hover:text-ukraine-yellow': variant === 'discreet' && color === 'yellow' && !disabled
-      })}
+      className={c(
+        {
+          'text-ukraine-blue': variant === 'normal' && color === 'blue',
+          'dark:text-blue-300': variant === 'normal' && color === 'blue',
+          'text-ukraine-yellow': variant === 'normal' && color === 'yellow',
+          'hover:underline': variant === 'normal' && !disabled,
+          'hover:text-ukraine-blue': variant === 'discreet' && color === 'blue' && !disabled,
+          'dark:hover:text-blue-300': variant === 'discreet' && color === 'blue' && !disabled,
+          'hover:text-ukraine-yellow': variant === 'discreet' && color === 'yellow' && !disabled
+        },
+        'cursor-pointer'
+      )}
       disabled={disabled}
       title={title}
       onClick={onClick}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ['**/*.jsx', '**/*.tsx'],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        'ukraine-blue': '#0057b7',
-        'ukraine-yellow': '#ffd700'
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
