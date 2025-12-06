4e4c94e840adea710ec7bbbf13546a9237e19d3f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-12-06 19:17:09 +0200

    Use dark theme by default

--- web/hooks/useTheme.ts
+++ web/hooks/useTheme.ts
@@ -3,20 +3,34 @@ import useLocalState from '~/hooks/useLocalState';
 import useMedia from '~/hooks/useMedia';
 
 const useTheme = () => {
-  const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)')
-    ? ('dark' as const)
-    : ('light' as const);
-
   const [userPreferredTheme, setUserPreferredTheme] = useLocalState<'light' | 'dark' | null>(
     'theme',
     null
   );
 
+  const systemPrefersDarkTheme = useMedia('(prefers-color-scheme: dark)');
+  const systemPrefersLightTheme = useMedia('(prefers-color-scheme: light)');
+  const systemPreferredTheme = useMemo<'light' | 'dark' | null>(() => {
+    if (systemPrefersDarkTheme) {
+      return 'dark';
+    }
+
+    if (systemPrefersLightTheme) {
+      return 'light';
+    }
+
+    return null;
+  }, [systemPrefersDarkTheme, systemPrefersLightTheme]);
+
   return useMemo(() => {
     return {
-      systemPreferredTheme,
       userPreferredTheme,
-      theme: userPreferredTheme || systemPreferredTheme,
+      systemPreferredTheme,
+      theme:
+        userPreferredTheme ||
+        systemPreferredTheme ||
+        // Default to dark to avoid flash banging users
+        'dark',
       setTheme: setUserPreferredTheme
     };
   }, [systemPreferredTheme, userPreferredTheme, setUserPreferredTheme]);
