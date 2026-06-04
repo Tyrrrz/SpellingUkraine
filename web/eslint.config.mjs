7f28fffc2e7ef3131f256b377d9a353cd711b103
tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2026-06-04 15:46:28 +0300

    Fix up eslint

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,17 +1,21 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
 export default defineConfig([
-  { ignores: ["out/", "dist/"] },
+  includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     plugins: {
+      react: reactPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
@@ -19,7 +23,9 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
+      ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
